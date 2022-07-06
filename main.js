454a9e98831bfe23da248472fb68e97239c628a3
Warish Alam <warishalam349@gmail.com>
2022-07-06 23:08:48 +0530

    NodeJs Calculator

---
+++ main.js
@@ -0,0 +1,10 @@
+const add =require("./add")
+const subtract =require("./subtract")
+const multiply =require("./multiply")
+const divide=require("./divide")
+
+
+console.log(add(5,9));
+console.log(subtract(10,4));
+console.log(multiply(5,5));
+console.log(divide(30,10));
